f89c45153acd272b20705ca117e0e5c386bf2a14
unknown <Jeisonlopezarroyave@gmail.com>
2024-04-14 21:41:12 -0500

    Jest

---
+++ challenge-testing/tests/test.test.js
@@ -0,0 +1,57 @@
+const CarritoCompra = require("../index");
+
+describe("CarritoCompra", () => {
+  let carrito;
+
+  beforeEach(() => {
+    carrito = new CarritoCompra();
+  });
+
+  test("agregarProducto agrega un producto al carrito", () => {
+    // Mock function para simular agregar un producto
+    const producto = { nombre: "Producto 1", precio: 10 };
+    const agregarProductoMock = jest.fn();
+    carrito.agregarProducto = agregarProductoMock;
+
+    carrito.agregarProducto(producto);
+
+    // Verificar que la función agregarProducto se haya llamado con el producto correcto
+    expect(agregarProductoMock).toHaveBeenCalledWith(producto);
+  });
+
+  test("calcularTotal llama al método calcularTotal correctamente", () => {
+    // Mock function para simular el cálculo del total
+    const calcularTotalMock = jest
+      .spyOn(carrito, "calcularTotal")
+      .mockReturnValue(50);
+
+    const total = carrito.calcularTotal();
+
+    // Verificar que el método calcularTotal haya sido llamado y retorne el valor esperado
+    expect(calcularTotalMock).toHaveBeenCalled();
+    expect(total).toBe(50);
+
+    // Restaurar el método calcularTotal original después de la prueba
+    calcularTotalMock.mockRestore();
+  });
+
+  test("aplicarDescuento aplica el descuento correctamente", () => {
+    // Mock function para simular aplicar un descuento
+    const porcentajeDescuento = 10;
+    const totalConDescuento = 90;
+    carrito.calcularTotal = jest.fn(() => 100); // Simula un total de compra de $100
+    const aplicarDescuentoMock = jest
+      .spyOn(carrito, "aplicarDescuento")
+      .mockReturnValue(totalConDescuento);
+
+    const totalConDescuentoCalculado =
+      carrito.aplicarDescuento(porcentajeDescuento);
+
+    // Verificar que el descuento se haya aplicado correctamente
+    expect(aplicarDescuentoMock).toHaveBeenCalledWith(porcentajeDescuento);
+    expect(totalConDescuentoCalculado).toBe(totalConDescuento);
+
+    // Restaurar el método aplicarDescuento original después de la prueba
+    aplicarDescuentoMock.mockRestore();
+  });
+});
